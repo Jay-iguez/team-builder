a89a1800cdd1883253bf3f46159bfc9df854ee63
Jayden Rodriguez <02rodriguez.j@gmail.com>
2023-07-15 22:14:30 -0700

    Import Axios/styled-components and create CSS basis for AppJS

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import styled from 'styled-components'
+import { useState, useEffect} from 'react';
+
+const mainBody = document.querySelector("body")
+mainBody.style.background = " #dad8cf "
+
+
+
+function App() {
+  return (
+    <h1>Hey</h1>
+  );
+}
+
+export default App;
